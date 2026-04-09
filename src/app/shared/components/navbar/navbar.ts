271fd7b777d1df35da9a8ad297362f2eb7cfa057
JuanEstepa.dev <juangoestepa@gmail.com>
2026-04-09 16:09:22 -0500

    Feat: create navbar and footer

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, ButtonModule, AvatarModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrl: './navbar.scss'
 })
-export class Navbar {}
+export class Navbar {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+
+  goToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+}
